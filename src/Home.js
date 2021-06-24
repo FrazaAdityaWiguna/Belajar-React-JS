92f0669d4d69ec3d48e4ffee009d74eb5b440d3f
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-24 10:09:22 +0700

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import navbar from './Navbar';
+
+const handleClick = (e) => {
+  console.log('Hello, Everyone!', e);
+};
+
+const handleClickAgain = (name, e) => {
+  console.log(`Hello, ${name}!`, e.target);
+};
 
 const Home = () => (
   <div className="home">
     <h2>Homepage</h2>
+    <button type="button" onClick={handleClick}>
+      Click me
+    </button>
+    <button type="button" onClick={(e) => handleClickAgain('Fraza', e)}>
+      Click me again!
+    </button>
   </div>
 );
 
