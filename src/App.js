1583ddb257d1d89fa9780a88d56b66787988c4cf
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-24 08:48:11 +0700

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'Welcome to the BigDream';
@@ -11,17 +13,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>My Portfolio</a>
-
-        {/* incorrect calling */}
-        {/* <p>{person}</p> */}
+        <Home />
       </div>
     </div>
   );
