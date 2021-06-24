f0a91ec4949c4ba684aa55701ce7f1f46fa3aab6
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-24 09:02:22 +0700

    Add style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the BigDream';
-  const likes = 1000;
-  const link = 'https://fraza-portfolio.web.app/';
-
-  // incorrect calling
-  // const person = { name: 'fraza', age: 19 };
-
   return (
     <div className="App">
       <Navbar />
