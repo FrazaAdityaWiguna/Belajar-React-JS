a2e2b0bbe8f6d62d141698de83de733ad6421f7d
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-26 17:30:31 +0700

    Route Parametes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BlogList = (props) => {
   const blogs = props;
@@ -9,8 +10,10 @@ const BlogList = (props) => {
       <h2>{title.title}</h2>
       {blogs.blogsHome.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
